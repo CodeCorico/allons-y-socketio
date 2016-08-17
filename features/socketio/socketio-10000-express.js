9461973de918580137dfdff25b7a977feee3df7a
Xavier Boubert <xavier@boubert.fr>
2016-08-17 16:29:40 +0200

    feat(socketio): create the feature

---
+++ features/socketio/socketio-10000-express.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = function($allonsy, $http) {
+  if (process.env.SOCKETIO && process.env.SOCKETIO == 'false') {
+    return;
+  }
+
+  var path = require('path'),
+      io = require('socket.io')($http),
+      socketIoCookieParser = require('socket.io-cookie-parser');
+
+  io.use(socketIoCookieParser(process.env.EXPRESS_COOKIE_SECRET));
+
+  DependencyInjection.service('$io', [function() {
+    return io;
+  }]);
+
+  var socketioFiles = $allonsy.findInFeaturesSync('*-socketio.js');
+
+  socketioFiles.forEach(function(file) {
+    DependencyInjection.injector.controller.invoke(null, require(path.resolve(file)));
+  });
+};
