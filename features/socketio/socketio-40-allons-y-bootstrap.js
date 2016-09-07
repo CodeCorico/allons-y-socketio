145ce994f524dd7cbb68fe34c158f931e9caae3a
Xavier Boubert <xavier@boubert.fr>
2016-09-07 12:49:55 +0200

    fix(socketio allons-y-bootstrap): check if EXPRESS is enabled

--- features/socketio/socketio-40-allons-y-bootstrap.js
+++ features/socketio/socketio-40-allons-y-bootstrap.js
@@ -4,29 +4,35 @@ var path = require('path');
 
 module.exports = {
   bootstrap: function($allonsy, $options, $done) {
-    if ((!process.env.SOCKETIO || process.env.SOCKETIO == 'true') && $options.owner == 'start') {
-      $allonsy.watcher('Allons-y Express', [
-        '*-socketio.js',
-        'controllers/*-event.js'
-      ]);
-
-      require(path.resolve(__dirname, 'socketio-service-back.js'))();
-
-      var $SocketIOService = DependencyInjection.injector.controller.get('$SocketIOService');
-
-      $allonsy.on('message', function(args) {
-        if (args.event == 'update(socketio/sockets)') {
-          $SocketIOService.processSockets(args.p, args.sockets, args.socketsReserved);
-        }
-        else if (args.event == 'call(socketio/url)') {
-          $allonsy.sendMessage({
-            event: args.event,
-            messageId: args.messageId,
-            url: $SocketIOService.socketsUrl()
-          });
-        }
-      });
+    if (
+      (process.env.EXPRESS && process.env.EXPRESS == 'false') ||
+      (process.env.SOCKETIO && process.env.SOCKETIO == 'false') ||
+      $options.owner != 'start'
+    ) {
+      return $done();
     }
+
+    $allonsy.watcher('Allons-y Express', [
+      '*-socketio.js',
+      'controllers/*-event.js'
+    ]);
+
+    require(path.resolve(__dirname, 'socketio-service-back.js'))();
+
+    var $SocketIOService = DependencyInjection.injector.controller.get('$SocketIOService');
+
+    $allonsy.on('message', function(args) {
+      if (args.event == 'update(socketio/sockets)') {
+        $SocketIOService.processSockets(args.p, args.sockets, args.socketsReserved);
+      }
+      else if (args.event == 'call(socketio/url)') {
+        $allonsy.sendMessage({
+          event: args.event,
+          messageId: args.messageId,
+          url: $SocketIOService.socketsUrl()
+        });
+      }
+    });
 
     $done();
   },
